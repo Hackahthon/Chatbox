9e94ae8baab87eef6bc76bea4654363ab4c8021a
Vladimir Ciobanu <admin@cvlad.info>
2012-10-27 22:10:53 +0300

    Updated Nuget references, created basic charbox.

---
+++ src/Chatbox.Web/SignalR/ChatboxHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace Chatbox.Web.SignalR
+{
+    [HubName("chatboxHub")]
+    public class ChatboxHub : Hub
+    {
+        public void Join()
+        {
+            var name = GetUsername();
+            Clients.onJoined(name);
+        }
+
+        public void Message(string content)
+        {
+            var user = GetUsername();
+            Clients.onMessage(user, content);
+        }
+
+        private string GetUsername()
+        {
+            var name = HttpContext.Current.User.Identity.Name;
+            return name;
+        }
+    }
+}
