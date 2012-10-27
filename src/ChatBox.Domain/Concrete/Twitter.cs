d6dfdc4bfaa8f911ea6305af6c2fc9ec04b2c175
Adrian Eduard <adrian.eduard@gmail.com>
2012-10-27 23:10:04 +0300

    a

---
+++ src/ChatBox.Domain/Concrete/Twitter.cs
@@ -0,0 +1,12 @@
+﻿using ChatBox.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBox.Domain.Concrete
+{
+    public class Twitter : ISocialize
+    {
+    }
+}
