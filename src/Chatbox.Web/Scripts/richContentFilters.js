73e15c8f70b3ff155bc0bb03cf411645e24d8832
Adrian Eduard <adrian.eduard@gmail.com>
2012-10-28 07:53:18 +0200

    u

--- src/Chatbox.Web/Scripts/richContentFilters.js
+++ src/Chatbox.Web/Scripts/richContentFilters.js
@@ -1,27 +1,50 @@
 ﻿function applyFilters(inputString) {
-    inputString = urlParser(inputString);
+    //inputString = parser(
+    //    inputString,
+    //    /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+    //    "url");
+    inputString = parser(
+        inputString,
+        /:\)/,
+        "emoticon");
     return inputString;
 }
 
-function urlParser(inputString) {
+function parser(inputString, expression, parseType) {
     var toReturn = "";
-    var expr = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expr);
+    var regex = new RegExp(expression);
 
     var foundIndex = inputString.search(regex);
     while (foundIndex > -1) {
-        var s = new String();
-        var foundUrl = inputString.match(expr);
+        var foundUrl = inputString.match(expression);
 
         toReturn += inputString.substr(0, foundIndex);
         inputString = inputString.substr(foundIndex, inputString.length - foundIndex);
 
-        var initialUrlLength = foundUrl[0].length;
-        if (foundUrl[0].indexOf("http://") < 0)
-            foundUrl[0] = "http://" + foundUrl[0];
+        var initialPatternLength = foundPattern[0].length;
+        switch (parseType) {
+            case "url":
+                if (foundPattern[0].indexOf("http://") < 0)
+                    foundPattern[0] = "http://" + foundPattern[0];
             
-        toReturn += '<a href="' + foundUrl[0] + '" target="_blank">' + foundUrl[0] + '</a>';
-        inputString = inputString.substr(initialUrlLength, inputString.length - initialUrlLength);
+                toReturn += '<a href="' + foundPattern[0] + '" target="_blank">' + foundPattern[0] + '</a>';
+                break;
+            case "emoticon":
+                switch (foundPattern[0].toUpperCase()) {
+                    case ":)":
+                        toReturn += '<img src="/Content/Images/Emoticons/smile.gif" style="vertical-align: absmiddle" />';
+                        break;
+                    case ":(":
+                        break;
+                    case ":D":
+                        break;
+                    case ":P":
+                        break;
+                }
+                break;
+        }
+
+        inputString = inputString.substr(initialPatternLength, inputString.length - initialPatternLength);
 
         foundIndex = inputString.search(regex);
 
