c19de19b9330a9aec4001d9fa443bc88e7f472da
Vladimir Ciobanu <admin@cvlad.info>
2012-10-28 03:23:18 +0200

    Split the chat javascript code a bit.

---
+++ src/Chatbox.Web/Scripts/ChatChannel.js
@@ -0,0 +1,16 @@
+﻿; (function (win, undefined) {
+
+    var chatService = null;
+
+    var chatChannel = function (chat, name) {
+        chatService = chat;
+
+        this.name = name;
+        this.sendMessage = function (message) {
+            chatService.message(name, message);
+        };
+    };
+
+    win.ChatChannel = chatChannel;
+
+})(window);
---
+++ src/Chatbox.Web/Scripts/ChatHost.js
@@ -0,0 +1,20 @@
+﻿; (function (win, undefined) {
+
+    var chat = $.connection.chatboxHub;
+
+    var channels = [];
+
+    var join = function (name) {
+        win.chatHost.activeChannel = new win.ChatChannel(chat, name);
+        channels.push(win.chatHost.activeChannel);
+        chat.join(name);
+    };
+
+    var chatHost = {
+        joinChannel: join,
+        activeChannel: null
+    };
+
+    win.chatHost = chatHost;
+
+})(window);
