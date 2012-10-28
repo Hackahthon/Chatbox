c6d23198017148d9961017cd293810da50b0a03e
Adrian Eduard <adrian.eduard@gmail.com>
2012-10-28 02:21:01 +0200

    test login

--- src/ChatBox.Domain/Concrete/Twitter.cs
+++ src/ChatBox.Domain/Concrete/Twitter.cs
@@ -3,10 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Twitterizer;
 
 namespace ChatBox.Domain.Concrete
 {
     public class Twitter : ISocialize
     {
+        public string Logon(Uri request, string returnUrl)
+        {
+            UriBuilder builder = new UriBuilder(request);
+            builder.Query = string.Concat(
+                builder.Query,
+                string.IsNullOrEmpty(builder.Query) ? string.Empty : "&",
+                "ReturnUrl=",
+                returnUrl);
+
+            string token = OAuthUtility.GetRequestToken(
+                "Bi1aDmdE4uftKsbWprnLA",
+                "Ym0GTfRiLp6qs6GJMmfcxylqcwZiUaSbyigiD2LdA",
+                builder.ToString()).Token;
+
+            return OAuthUtility.BuildAuthorizationUri(token, true).ToString();
+        }
     }
 }
---
+++ src/Chatbox.Web/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chatbox.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Logon()
+        {
+            ChatBox.Domain.Concrete.Twitter twitter = new ChatBox.Domain.Concrete.Twitter();
+            return Redirect(twitter.Logon(this.Request.Url, "http://chatbox.azurewebsites.net/"));
+        }
+    }
+}
