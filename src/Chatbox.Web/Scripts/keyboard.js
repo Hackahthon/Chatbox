ddab8cf04a3bbcb5b5757584c67c0d2f4bbaf9fc
Vladimir Ciobanu <admin@cvlad.info>
2012-10-28 07:43:16 +0200

    Keyboard support.

---
+++ src/Chatbox.Web/Scripts/keyboard.js
@@ -0,0 +1,9 @@
+﻿; (function (win, undefined) {
+
+    for (var i = 0; i < 10; ++i) {
+        Mousetrap.bind('ctrl+' + i, function () {
+            $('#tabs').tabs({ active: i });
+        });
+    }
+
+})(window);
